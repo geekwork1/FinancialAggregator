9bd2c21391359bee1d6308a7a2b1e7a7165f1cf9
Raider992 <19stargazer92@gmail.com>
2022-03-16 17:57:19 +0300

    Corrected doubling imports in settings.py

--- finaggregator/finaggregator/settings.py
+++ finaggregator/finaggregator/settings.py
@@ -11,10 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-try:
-    from finaggregator.settings_local_finaggregator import *
-except ImportError:
-    print('Not file settings_local_finaggregator.py. Added this file.')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
