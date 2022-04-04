21769663e72c019a1b9328a0a18d5def81605525
Raider992 <19stargazer92@gmail.com>
2022-04-04 18:40:23 +0300

    initial_commit

--- finaggregator/finaggregator/urls.py
+++ finaggregator/finaggregator/urls.py
@@ -25,7 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('auth/', include('djoser.urls')),
-    # path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls.jwt')),
-    # path("api/usersapp/", include("usersapp.urls"))
 ]
